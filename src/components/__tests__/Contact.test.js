156883f09e40ead7d464db397258af504e8dc5ae
74716 <ndeb2450@gmail.com>
2024-06-27 19:58:31 +0530

    basic changes describe function added and used it instead of test

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,16 +2,19 @@ import { render,screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test("To Check whether h1 tag is loaded", ()=>{
-    render(<Contact />);
-    const heading = screen.getByRole("heading", {level: 1});
-    expect(heading).toBeInTheDocument();
-});
+describe("Testing Loading of Contact us Page", ()=>{
+    it("To Check whether h1 tag is loaded", ()=>{
+        render(<Contact />);
+        const heading = screen.getByRole("heading", {level: 1});
+        expect(heading).toBeInTheDocument();
+    });
+    
+    it("To Check whether h4 call tag is loaded", ()=>{
+        render(<Contact />);
+       const heading = screen.getByRole("heading", {level: 4, name: /Call/i });
+       console.log(heading);
+        expect(heading).toBeInTheDocument();
+    });
+})
 
-test("To Check whether h4 call tag is loaded", ()=>{
-    render(<Contact />);
-   const heading = screen.getByRole("heading", {level: 4, name: /Call/i });
-   console.log(heading);
-    expect(heading).toBeInTheDocument();
-});
 
